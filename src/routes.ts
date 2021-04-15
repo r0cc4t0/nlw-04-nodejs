1a5d2b2f51bad80a1639eca5c0ef292b2646448d
r0cc4t0 <guilherme.roccato@outlook.com>
2021-04-14 23:45:30 -0300

    Survey Creation

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import UserController from './controllers/UserController';
+import SurveyController from './controllers/SurveyController';
 
 const routes = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 routes.post('/users', userController.create);
+routes.post('/surveys', surveyController.create);
 
 export default routes;
