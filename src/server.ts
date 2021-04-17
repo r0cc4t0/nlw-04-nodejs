23fe43d7f3c3960f0c3da83e0481b0c37880431f
r0cc4t0 <guilherme.roccato@outlook.com>
2021-04-16 23:58:42 -0300

    Users Tests

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-import express from 'express';
-import 'reflect-metadata';
-import './database';
-import routes from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
+import app from './app';
 
 app.listen(3333, () => console.log('Server is running!'));
